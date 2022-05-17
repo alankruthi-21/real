e181b4c1dd7ae7bc31147629bff31ae588430ee2
alankruthi-21 <2215316621@gitam.in>
2022-05-17 16:12:27 +0530

    deploy added

---
+++ jenkinsfile-deploy.groovy
@@ -0,0 +1,32 @@
+pipeline{
+    agent any
+   parameters {
+    string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'From which branch artifacts want to deploy?')
+    string(name: 'BUILD_NUM', defaultValue: '', description: 'From which build number artifacts want to deploy?')
+    string(name: 'SERVER_IP', defaultValue: '', description: 'To  which want to deploy?')
+
+  }
+    stages{
+        stage("download artifact"){
+            steps {
+                println "Here I'm downloading artifacts from S3"
+                sh """
+                        aws s3 ls
+                        aws s3 ls s3://alankruthiart
+                        aws s3 ls s3://alankruthiart/${BRANCH_NAME}/${BUILD_NUM}/
+                        aws s3 cp s3://alankruthiart/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war .
+ 
+                   """
+                
+            }
+        }
+        stage("copy artifacts") {
+            steps {
+                println "Here I'm coping artifact from Jenkins to Tomcat servers"
+                 sh "scp -o StrictHostKeyChecking=no -i /tmp/alankruthi21.pem hello-${BUILD_NUM}.war ec2-user@${SERVER_IP}:/tmp"
+                 sh "ssh -o StrictHostKeyChecking=no -i /tmp/alankruthi21.pem ec2-user@${SERVER_IP} \"sudo cp /tmp/hello-${BUILD_NUM}.war /var/lib/tomcat/webapps\""
+               
+            }
+        }
+    }
+}
